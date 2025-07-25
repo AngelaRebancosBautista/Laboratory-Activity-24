007be3561304ce7a4214771b094fe93df8f7b3ed
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 12:49:55 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string correctUsername = "admin";
+            const string correctPassword = "1234";
+
+            bool isAuthenticated = false;
+
+            while (!isAuthenticated)
+            {
+                Console.Write("Enter username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+
+                if (username == correctUsername && password == correctPassword)
+                {
+                    Console.WriteLine("Login successful");
+                    isAuthenticated = true;
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    Console.WriteLine();  
+                }
+            }
+        }
+    }
+}
+   
+
